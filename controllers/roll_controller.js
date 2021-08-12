ff174beba0053acc940cd8e0f0522f275a5619bb
Mbwata <housems@hotmail.com>
2021-08-12 16:00:23 +0100

    well it works on heroku

--- controllers/roll_controller.js
+++ controllers/roll_controller.js
@@ -1,20 +1,21 @@
 const express = require('express');
 const router = express.Router();
 const Die = require('../models/die');
-const { db } = require('../lib/db');
+//const { db } = require('../lib/db');
 const { isEnabled } = require('unleash-client');
 
 router.get('/', async (req, res) => {
+  console.log("rolling...");
   let die = new Die(req.query.sides);
   let roll = die.roll();
   let message = `A die with ${die.sides} sides just rolled a ${roll}.`;
-  await saveRoll(die.sides, roll);
+  //await saveRoll(die.sides, roll);
 
   if (isEnabled('doubleroll')) {
     let die2 = new Die(req.query.sides);
     let roll2 = die2.roll();
     message = `Two dice each with ${die.sides} sides just rolled a ${roll} and ${roll2}.`;
-    await saveRoll(die2.sides, roll2);
+    //await saveRoll(die2.sides, roll2);
   }
 
   res.send(message)
